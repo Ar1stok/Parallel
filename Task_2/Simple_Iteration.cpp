bbfc6cd08ac3e56753c8e29b1f168509151c4676
Ar1stok <p.krylov03@mail.ru>
2024-02-28 15:35:13 +0000

    update

--- Task_2/Simple_Iteration.cpp
+++ Task_2/Simple_Iteration.cpp
@@ -3,56 +3,75 @@
 #include <chrono>
 #include <omp.h>
 
-#define arr_elem 500
-#define numThreads 1
-#define MAX_ITERATIONS 100000
+#define arr_elem 5000
+#define numThreads 40
 
 int main(int argc, char const* argv[]) 
 {
 
-    double *matrix, *vector_b;
+    double *matrix, *vector_b, *vector_x;
     matrix = new double[arr_elem * arr_elem];
     vector_b = new double[arr_elem];
-    double x[arr_elem] = {0.0};
-    
-    for (int i = 0; i < arr_elem * arr_elem; i++)
+    vector_x = new double[arr_elem];
+
+    double eps = 0.00001;
+    double t = 0.00001;
+
+    auto begin = std::chrono::steady_clock::now();
+    #pragma omp parallel num_threads(numThreads)
     {
-        matrix[i] = (i % (arr_elem + 1) == 0) ? 2.0 : 1.0;
-    }
-
-    for (int i = 0; i < arr_elem; i++)
-    {
-        vector_b[i] = double(arr_elem) + 1.0;
-    }
-
-    double eps = 0.001;
-
-    for (int k = 0; k < MAX_ITERATIONS; ++k) 
-    {
-        double error = 0;
-        double temp;
-
-        for (int i = 0; i < arr_elem; i++) 
+        #pragma omp for
+        for (int i = 0; i < arr_elem * arr_elem; i++)
         {
-            temp = x[i];
-            x[i] = 0;
-            for (int j = 0; j < arr_elem; j++) 
+            matrix[i] = (i % (arr_elem + 1) == 0) ? 2.0 : 1.0;
+        }
+
+        #pragma omp for
+        for (int i = 0; i < arr_elem; i++)
+        {
+            vector_b[i] = double(arr_elem) + 1.0;
+        }
+
+        #pragma omp for
+        for (int i = 0; i < arr_elem; i++)
+        {
+            vector_x[i] = 0.0;
+        }
+        
+        double temp_values[arr_elem] = {0.0};
+        
+        while (true) {
+
+            #pragma omp for
+            for (int i = 0; i < arr_elem; ++i) 
             {
-                if (i != j)
+                double sum = 0;
+                for (int j = 0; j < arr_elem; ++j) 
                 {
-                    x[i] += matrix[i * arr_elem + j] * x[j];
+                    sum += matrix[i * arr_elem + j] * temp_values[j];
                 }
+                vector_x[i] = temp_values[i] - t * (sum - vector_b[i]);
             }
-            x[i] = (vector_b[i] - x[i]) / matrix[i * arr_elem + i];
-            error = std::max(error, std::abs(temp - x[i]));
-        }
-
-        if (error < eps)
-        {
-            std::cout << k << "\n";
-            break;
+            
+            if (std::abs(vector_x[0] - temp_values[0]) < eps)
+            {
+                break;
+            }
+            
+            for (int i = 0; i < arr_elem; i++)
+            {
+                temp_values[i] = vector_x[i];
+            }
         }
     }
 
+    auto end = std::chrono::steady_clock::now();
+    auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+
+    std::cout << "Vector: " << vector_x[0];
+
+    std::cout << "\n The time: " << elapsed_ms.count() << " ms\n";
+
+    delete (matrix, vector_b, vector_x);
     return 0;
 }
