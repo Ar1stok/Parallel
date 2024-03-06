5394fd0bc2d432e167a206d9543fc18131d96752
Ar1stok <p.krylov03@mail.ru>
2024-03-06 13:24:09 +0000

    correction

--- Task_2/Simple_Iteration.cpp
+++ Task_2/Simple_Iteration.cpp
@@ -4,17 +4,20 @@
 #include <omp.h>
 #include <memory>
 
-#define arr_elem 5000
-#define numThreads 40
+#define arr_elem 1000
+#define numThreads 16
 
 int main(int argc, char const* argv[]) 
 {
     std::shared_ptr<double[]> matrix(new double[arr_elem * arr_elem]);
     std::shared_ptr<double[]> vector_x(new double[arr_elem]);
     std::shared_ptr<double[]> vector_b(new double[arr_elem]);
+    std::shared_ptr<double[]> temp_values(new double[arr_elem]);
 
     double eps = 0.00001;
     double t = 0.00001;
+    double term = 0.0;
+    double norm_v_b = sqrt((double(arr_elem) + 1.0) * (double(arr_elem) + 1.0) * double(arr_elem));
 
     auto begin = std::chrono::steady_clock::now();
     #pragma omp parallel num_threads(numThreads)
@@ -36,8 +39,12 @@ int main(int argc, char const* argv[])
         {
             vector_x[i] = 0.0;
         }
-        
-        double temp_values[arr_elem] = {0.0};
+
+        #pragma omp for
+        for (int i = 0; i < arr_elem; i++)
+        {
+            temp_values[i] = 0.0;
+        }
         
         while (true) {
 
@@ -52,7 +59,21 @@ int main(int argc, char const* argv[])
                 vector_x[i] = temp_values[i] - t * (sum - vector_b[i]);
             }
             
-            if (std::abs(vector_x[0] - temp_values[0]) < eps)
+            // Критерий остановки
+            term = 0.0;
+
+            #pragma omp for
+            for (int i = 0; i < arr_elem; ++i) 
+            {
+                double sum = 0;
+                for (int j = 0; j < arr_elem; ++j) 
+                {
+                    sum += matrix[i * arr_elem + j] * vector_x[j];
+                }
+                term += (sum - vector_b[i]) * (sum - vector_b[i]);
+            }
+
+            if (std::abs(sqrt(term) / norm_v_b) < eps)
             {
                 break;
             }
